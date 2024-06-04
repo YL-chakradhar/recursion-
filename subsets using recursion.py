eb8a37bb87cb1319e804812a0cec446fe2ad42b8
YL-chakradhar <yerralakshmichakradhar5@gmail.com>
2024-06-04 11:32:32 +0530

    third commit

--- subsets using recursion.py
+++ subsets using recursion.py
@@ -1,12 +1,37 @@
-def subset(i,res,a):
-    if i==len(a):
-        print(res)
-        return
-    res.append(a[i])
-    subset(i+1,res,a)
-    res.pop()
-    subset(i+1,res,a)
+#method 1
+import itertools
 
-a=[1,2,3,4,5]
-res=[]
-print(subset(0,res,a))
+def generate_subsets_itertools(a):
+    subsets = []
+    for i in range(len(a) + 1):
+        subsets.extend(itertools.combinations(a, i))
+    return subsets
+
+a = [1, 2, 3, 4, 5]
+subsets = generate_subsets_itertools(a)
+for subset in subsets:
+    print(list(subset))
+
+
+#**method 2
+
+def printAllSubsets(nums, index, result, value):
+    if index == len(nums):
+        print(result)
+        value[0] += 1
+        return
+ 
+    # Include 
+    result.append(nums[index])
+    printAllSubsets(nums, index + 1, result, value)
+    result.pop()
+ 
+    # Exclude
+    printAllSubsets(nums, index + 1, result, value)
+ 
+ 
+nums = [10, 20, 30, 40, 50, 60, 65]
+ 
+value = [0]
+printAllSubsets(nums, 0, [], value)
+print(value[0])
